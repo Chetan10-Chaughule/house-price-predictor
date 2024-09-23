6b31eff4cdb54171b57faee476c475f2b25fb625
Chetan10-Chaughule <125849349+Chetan10-Chaughule@users.noreply.github.com>
2024-09-24 00:42:26 +0530

    Add files via upload

---
+++ bhp/client/script.js
@@ -0,0 +1,50 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementById("uiBathrooms");
+    return parseInt(uiBathrooms.value);
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementById("uiBHK");
+    return parseInt(uiBHK.value);
+}
+
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://127.0.0.1:5000/predict_home_price"; // Use this if you are NOT using nginx
+
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value
+    }, function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+}
+
+function onPageLoad() {
+    console.log("document loaded");
+    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx
+    $.get(url, function(data, status) {
+        console.log("got response for get_location_names request");
+        if (data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for (var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
